06168b0f167fc01674701771a8ef50b34e0f612e
camlebrun <c_lebrun3@hetic.eu>
2023-02-08 14:15:12 +0100

    12

--- app/pages/6_categorie.py
+++ app/pages/6_categorie.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 st.title("Vente par catégorie")
-df = pd.read_csv("Data/for_streamlit/finale.csv")
+df = pd.read_csv("for_streamlit/finale.csv")
 fig = px.bar(df, x="Categorie", y="count(Montant)", labels={'count(Montant)':'Total des ventes'})
 
 st.plotly_chart(fig, use_container_width = True)
